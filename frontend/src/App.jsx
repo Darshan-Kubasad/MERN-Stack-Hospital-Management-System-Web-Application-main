99faa366bbcbfd4ad800d3b864f20097cd2cd5f7
darshan-kubasad <darshankubasad777@gmail.com>
2025-12-26 18:24:22 +0530

    final

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -24,7 +24,7 @@ const App = () => {
     const fetchUser = async () => {
       try {
         const response = await axios.get(
-          `${API_URL}/v1/user/patient/me`,
+          `${API_URL}/api/v1/user/patient/me`,
           {
             withCredentials: true,
           }
